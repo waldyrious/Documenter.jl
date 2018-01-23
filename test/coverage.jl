c050ca88c528f6a83d522ee94d43e7a0a4fdf474
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-01-23 18:58:32 +1300

    Upload to Codecov from 0.6 builds
    The Coverage package appears to be broken on the nightly.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,6 +1,6 @@
-# Only run coverage from linux nightly build on travis.
+# Only run coverage from Julia 0.6 Linux build on Travis.
 get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.6" || exit()
 
 Pkg.add("Coverage")
 using Coverage
